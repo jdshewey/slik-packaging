bb27da6759b2efafdf3cdb606d4da273d43b41e5
jdshewey <jdshewey@gmail.com>
2018-05-28 00:14:39 -0600

    using my fork until upstream PRs merged.

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ sudo mkdir /opt/slik-packaging
 cd /opt/slik-packaging
 yum -y install rpm-build
 git clone https://github.com/jdshewey/slik
-git clone https://github.com/salt-formulas/salt-formula-freeipa
-git clone https://github.com/salt-formulas/salt-formula-openssh
+git clone https://github.com/jdshewey/salt-formula-freeipa
+git clone https://github.com/jdshewey/salt-formula-openssh.git
 git clone https://github.com/jdshewey/pulp_hook
 rm -f ./rpmbuild/SOURCES/slik.tar.gz
-tar -czvf ./rpmbuild/SOURCES/slik.tar.gz slik/
+tar -czvf ./rpmbuild/SOURCES/slik.tar.gz --exclude=".git" slik/
